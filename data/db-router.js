725ee8ad3a3d914cb5f82741cce7016399a984b4
Chance Rutledge <chancerutledge2@gmail.com>
2020-03-11 08:56:07 -0700

    5/7

--- data/db-router.js
+++ data/db-router.js
@@ -86,6 +86,20 @@ router.put('/:id', (req, res) => {
 
 });
 
+router.delete('/:id', (req, res) => {
+    Db.remove(req.params.id)
+    .then(count => {
+        if(count > 0){
+            res.status(200).json({ message: "the message has been deleted"});
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist."});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: "The post could not be removed" });
+    })
+})
 
 
 
